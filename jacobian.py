7969647131c3936ae4be8fe5d9eba79f2ab72541
abhishek raj dutta <abhishek.eed.svnit@gmail.com>
2018-12-06 00:51:48 -0500

    bad jacobian

--- jacobian.py
+++ jacobian.py
@@ -11,18 +11,36 @@ def compose(*poses):
     """Compose all Pose2 transforms given as arguments from left to right."""
     return reduce((lambda x, y: x.compose(y)), poses)
 
-q = [0,0,0,0,0,0,0]
 
 si = []
 si.append(np.array([0,0,1,0,0,0], dtype=np.float))
-si.append (np.array([0,1,0,0,0,1], dtype=np.float))
+si.append (np.array([0,-1,0,0,0,-1], dtype=np.float))
 si.append (np.array([1,0,0,0,0,0], dtype=np.float))
-si.append (np.array([0,1,0,0,0,3], dtype=np.float))
+si.append (np.array([0,-1,0,0,0,-3], dtype=np.float))
 si.append (np.array([1,0,0,0,0,0], dtype=np.float))
-si.append (np.array([0,1,0,0,0,5], dtype=np.float))
+si.append (np.array([0,-1,0,0,0,-5], dtype=np.float))
 si.append (np.array([1,0,0,0,0,0], dtype=np.float))
 
-# link 1
+# print Pose3(Rot3.rpy(0.0, 0.0, 0), Point3(0, 0, 0))
+
+v = np.array([[0,0,0,0,1,0]]).T
+q = [0,0,0,0,0,0,0]
+# def get_T(q):
+L = [1, 1, 1, 1, 1, 1, 1]
+T=[]
+T.append(Pose3(Rot3.Ypr(q[0], 0.0, 0.0 ), Point3(0, 0, 0)))
+T.append(Pose3(Rot3.Ypr(0.0, q[1], 0.0), Point3(L[0], 0, 0)))
+T.append(Pose3(Rot3.Ypr(0.0, 0.0, q[2]), Point3(L[1], 0, 0)))
+T.append(Pose3(Rot3.Ypr(0.0, q[3],0.0 ), Point3(L[2], 0, 0)))
+T.append(Pose3(Rot3.Ypr(0.0, 0.0 ,q[4] ), Point3(L[3], 0, 0)))
+T.append(Pose3(Rot3.Ypr(0.0, q[5],0.0 ), Point3(L[4], 0, 0)))
+T.append(Pose3(Rot3.Ypr(0.0, 0.0 ,q[6] ), Point3(L[5], 0, 0)))
+T.append(Pose3(Rot3.Ypr(0.0, 0.0 , 0.0 ), Point3(L[6], 0, 0)))
+
+fk = Pose3(Rot3.Ypr(0.0, 0.0, 0.0), Point3(0, 0, 0))
+
+
+# forward kinematics
 g = []
 g.append ( Pose3.Expmap(q[0]*np.array([0,0,1,0,0,0], dtype=np.float)))
 g.append ( Pose3.Expmap(q[1]*np.array([0,1,0,0,0,1], dtype=np.float)))
@@ -34,23 +52,58 @@ g.append ( Pose3.Expmap(q[6]*np.array([1,0,0,0,0,0], dtype=np.float)))
 gtool =  Pose3(Rot3.Ypr(0.0, 0.0, 0.0), Point3(7, 0, 0))
 
 fk = Pose3(Rot3.Ypr(0.0, 0.0, 0.0), Point3(0, 0, 0))
-
-for i in range(0,7):
-	fk = compose(fk,g[i])
-
-
-# print fk.Adjoint(np.array([0,0,1,0,0,0], dtype=np.float))
-
+# 
 J = np.array([[0,0,0,0,0,0]]).T
-print J
 for i in range(0,7):
 	G = Pose3(Rot3.Ypr(0.0, 0.0, 0.0), Point3(0, 0, 0))
 	for j in range (0,i):
-		G = compose(G,g[j])
+		G = compose(G,T[j])
 
 	if i == 0:
 		J = np.c_[J,np.expand_dims(si[i], axis=1)]
 	else:		
 		J = np.c_[J,np.expand_dims(G.Adjoint(si[i]), axis=1)]
 
+J = np.delete(J, 0, 1)
+Jinv = np.linalg.pinv(J)
+d = np.matmul(Jinv,v)
 print J
+print Jinv
+
+q = q+np.squeeze(d)
+
+print q
+
+del T[:]
+T.append(Pose3(Rot3.Ypr(q[0], 0.0, 0.0 ), Point3(0, 0, 0)))
+T.append(Pose3(Rot3.Ypr(0.0, q[1], 0.0), Point3(L[0], 0, 0)))
+T.append(Pose3(Rot3.Ypr(0.0, 0.0, q[2]), Point3(L[1], 0, 0)))
+T.append(Pose3(Rot3.Ypr(0.0, q[3],0.0 ), Point3(L[2], 0, 0)))
+T.append(Pose3(Rot3.Ypr(0.0, 0.0 ,q[4] ), Point3(L[3], 0, 0)))
+T.append(Pose3(Rot3.Ypr(0.0, q[5],0.0 ), Point3(L[4], 0, 0)))
+T.append(Pose3(Rot3.Ypr(0.0, 0.0 ,q[6] ), Point3(L[5], 0, 0)))
+T.append(Pose3(Rot3.Ypr(0.0, 0.0 , 0.0 ), Point3(L[6], 0, 0)))
+
+fk = Pose3(Rot3.Ypr(0.0, 0.0, 0.0), Point3(0, 0, 0))
+
+for i in range(0,8):
+	fk = compose(fk,T[i])
+print fk
+
+
+# # test fk
+# g.append ( Pose3.Expmap(q[0]*np.array([0,0,1,0,0,0], dtype=np.float)))
+# g.append ( Pose3.Expmap(q[1]*np.array([0,1,0,0,0,1], dtype=np.float)))
+# g.append ( Pose3.Expmap(q[2]*np.array([1,0,0,0,0,0], dtype=np.float)))
+# g.append ( Pose3.Expmap(q[3]*np.array([0,1,0,0,0,3], dtype=np.float)))
+# g.append ( Pose3.Expmap(q[4]*np.array([1,0,0,0,0,0], dtype=np.float)))
+# g.append ( Pose3.Expmap(q[5]*np.array([0,1,0,0,0,5], dtype=np.float)))
+# g.append ( Pose3.Expmap(q[6]*np.array([1,0,0,0,0,0], dtype=np.float)))
+# gtool =  Pose3(Rot3.Ypr(0.0, 0.0, 0.0), Point3(7, 0, 0))
+
+# fk = Pose3(Rot3.Ypr(0.0, 0.0, 0.0), Point3(0, 0, 0))
+
+# for i in range(0,7):
+# 	fk = compose(fk,g[i])
+
+# print compose(fk,gtool)
