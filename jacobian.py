Fix Jacobian Ordering.

jacobian.py
@@ -14,12 +14,13 @@ def compose(*poses):
 FETCH_WHEEL_RADIUS = 0.055325
 FETCH_WHEEL_BASE = 2 * 0.18738
 
-base_jacobian = np.array([[1, 1], #x
+base_jacobian = np.array([[0, 0], #th_x
+													[0, 0], #th_y
+													[1/FETCH_WHEEL_BASE, -1/FETCH_WHEEL_BASE]  #th_z
+													[1, 1], #x
 													[0, 0], #y
 													[0, 0], #z
-													[0, 0], #th_x
-													[0, 0], #th_y
-													[1/FETCH_WHEEL_BASE, -1/FETCH_WHEEL_BASE]]) * FETCH_WHEEL_RADIUS #th_z
+													]) * FETCH_WHEEL_RADIUS
 
 si = []
 si.append(np.array([0,0,1,0,0,0], dtype=np.float))